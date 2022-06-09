e629bc0f93fd4626054704cefa7ce444bbf55bab
Giancarlo Lester <gian.lepear@gmail.com>
2022-06-09 01:04:24 -0300

    Correção do script do pre-commit

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo("Exportando dev requirements")
+echo "Exportando dev requirements"
 poetry export --dev -f  requirements.txt --without-hashes > requirements.dev.txt
 
-echo("Exportando prod requirements")
+echo "Exportando prod requirements"
 poetry export -f  requirements.txt --without-hashes > requirements.txt
